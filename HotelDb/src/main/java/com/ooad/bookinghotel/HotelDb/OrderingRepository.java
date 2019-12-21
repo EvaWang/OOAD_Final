b40b869eaacb8660acf4ecb96875a43f7f462872
Mikie0520 <55621978+Mikie0520@users.noreply.github.com>
2019-12-21 10:12:21 +0800

    Create OrderingRepository.java

---
+++ HotelDb/src/main/java/com/ooad/bookinghotel/HotelDb/OrderingRepository.java
@@ -0,0 +1,7 @@
+package com.ooad.bookinghotel.HotelDb;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface OrderingRepository extends CrudRepository<Ordering, Integer>{
+
+}
