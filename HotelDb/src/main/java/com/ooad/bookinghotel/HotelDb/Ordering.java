b922d033b32f69041c2b812ce9e9813980ed055c
Mikie0520 <55621978+Mikie0520@users.noreply.github.com>
2019-12-21 10:00:25 +0800

    Changed

---
+++ HotelDb/src/main/java/com/ooad/bookinghotel/HotelDb/Ordering.java
@@ -0,0 +1,48 @@
+package com.ooad.bookinghotel.HotelDb;
+
+import javax.persistence.*;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Ordering extends BaseDbo{
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @Column(updatable = false, nullable = false)
+    private Integer id;
+
+    @Column(updatable = false, nullable = false)
+    private Integer BookingId;
+
+    @Column(updatable = false, nullable = false)
+    private Integer UserId;
+
+    @Column(columnDefinition = "varchar(255) default 99999", nullable = false)
+    private Integer Total;
+
+    @Column(columnDefinition = "varchar(255) default 1", nullable = false)
+    private Double Discount;
+
+    @Column(columnDefinition = "varchar(500) default 0", nullable = false)
+    private String Memo;
+
+    public Integer getId() {return id;}
+
+    public Integer getBookingId() { return BookingId;}
+
+    public void setBookingId(Integer BookingId){this.BookingId = BookingId;}
+
+    public Integer getUserId() { return UserId;}
+
+    public void setUserId(Integer UserId) {this.UserId = UserId;}
+
+    public Integer getTotal() { return Total;}
+
+    public void setTotal(Integer Total) {this.Total = Total;}
+
+    public Double getDiscount() { return Discount;}
+
+    public void setDiscount(Double Discount) {this.Discount = Discount;}
+
+    public String getMemo() { return Memo;}
+
+    public void setMemo(String Memo) { this.Memo = Memo; }
+}
