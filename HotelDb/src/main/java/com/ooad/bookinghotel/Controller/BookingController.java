2fbfab560174ae4a311a6342b1bd807a8ca32c06
Lee Cindy <r5713410@gmail.com>
2019-12-23 18:58:12 +0800

    - testAdd 改至 Ordering/add

--- HotelDb/src/main/java/com/ooad/bookinghotel/Controller/BookingController.java
+++ HotelDb/src/main/java/com/ooad/bookinghotel/Controller/BookingController.java
@@ -22,11 +22,6 @@ public class BookingController {
 
     @Autowired
     private BookingRepository bookingRepository;
-    @Autowired
-    private OrderingRepository orderingRepository;
-    @Autowired
-    private HotelRoomRepository hotelRoomRepository;
-
 
 //    debug use
 //    @GetMapping("/all")
@@ -78,64 +73,5 @@ public class BookingController {
                     return bookingRepository.save(updateBooking);
                 }).orElseThrow(() -> new NotFoundException(id));
 
-    }
-
-    @PostMapping(path="/testAdd", consumes = "application/json")
-    Ordering newOrdering(@RequestBody Map<String, String> orderingObj) throws ParseException {
-
-        SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy-MM-dd");
-        Date startDate = formatter1.parse(orderingObj.get("StartDate"));
-        Date endDate = formatter1.parse(orderingObj.get("EndDate"));
-
-        Ordering newOrdering = new Ordering();
-        newOrdering.setUserId(Integer.parseInt(orderingObj.get("UserId")));
-        newOrdering.setDiscount(Double.parseDouble(orderingObj.get("Discount")));
-        newOrdering.setMemo(orderingObj.get("Memo"));
-        //return OrderingRepository.findByHotelIdAndRoomType(HotelId, RoomType)
-        //       .orElseThrow(() -> new NotFoundException(HotelId, RoomType));
-        String HotelRoomIds = orderingObj.get("HotelRoomIds");
-        String[] roomIdList = HotelRoomIds.split(",");
-
-        ArrayList bookingArray = new ArrayList();
-        Integer hotelId = Integer.parseInt(orderingObj.get("HotelId"));
-
-        List<String> roomIdList_toList = Arrays.asList(roomIdList);
-
-        List<Integer> int_roomIdList_toList = new ArrayList<>();
-        for(String s : roomIdList_toList) int_roomIdList_toList.add(Integer.valueOf(s));
-
-        List<HotelRoom> roomIdList_checked = new ArrayList<>();
-        roomIdList_checked =  hotelRoomRepository.findByRoomIds(int_roomIdList_toList, hotelId);
-        Integer Total = 0;
-
-        Dictionary<Integer, Integer> roomDict = new Hashtable();
-        for(HotelRoom item : roomIdList_checked){
-            roomDict.put(item.getId(), item.getPrice());
-        }
-        Integer roomPrice = 0;
-        for(Integer i : int_roomIdList_toList){
-            //System.out.println(roomDict.get(i));
-            roomPrice = roomDict.get(i);
-            Total = Total + roomPrice;
-        }
-
-        newOrdering.setTotal(Total);
-        newOrdering = orderingRepository.save(newOrdering);
-
-        for(String roomId: roomIdList){
-            Booking newBooking = new Booking();
-            newBooking.setHotelId(hotelId);
-            newBooking.setHotelRoomId(Integer.parseInt(roomId));
-            newBooking.setOrderId(newOrdering.getId());
-            newBooking.setStartDate(startDate);
-            newBooking.setEndDate(endDate);
-            newBooking.setIsDisabled(Boolean.getBoolean(orderingObj.get("IsDisabled")));
-            //bookingRepository.save(newBooking);
-            bookingArray.add(newBooking);
-        }
-
-         bookingRepository.saveAll(bookingArray);
-
-        return newOrdering;
     }
 }
