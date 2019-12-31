6493efc4397eca8e1f8edca26c252561b3c72976
Mikie0520 <55621978+Mikie0520@users.noreply.github.com>
2019-12-31 10:20:13 +0800

    Merge branch 'master' of https://github.com/EvaWang/OOAD_Final
    # Conflicts:
    #	HotelDb/src/main/java/com/ooad/bookinghotel/HotelDb/Ordering.java

--- HotelDb/src/main/java/com/ooad/bookinghotel/Controller/OrderingController.java
+++ HotelDb/src/main/java/com/ooad/bookinghotel/Controller/OrderingController.java
@@ -177,4 +177,22 @@ public class OrderingController {
         return originalOrdering;
     }
 
+    /*@PutMapping
+    Ordering updateOrderByBooking(@RequestBody Map<String, String> orderingObj,@PathVariable int id) {
+        Ordering originalOrdering = orderingRepository.findById(id).get();
+
+        if (originalOrdering.getIsPaid() == true) {
+            System.out.println("This order has been Paid");
+            return originalOrdering;
+        }
+
+        if (originalOrdering.getIsDisabled() == true) {
+            System.out.println("This order has been disabled");
+            return originalOrdering;
+        }
+
+
+        return originalOrdering;
+    }*/
+
 }
--- HotelDb/src/main/java/com/ooad/bookinghotel/HotelDb/Ordering.java
+++ HotelDb/src/main/java/com/ooad/bookinghotel/HotelDb/Ordering.java
@@ -36,9 +36,6 @@ public class Ordering extends BaseDbo{
     @Column(columnDefinition = "varchar(500) default 0", nullable = false)
     private String Memo;
 
-    @Column(nullable = false)
-    private Boolean isPaid;
-
     public Integer getId() { return id; }
 
     //public Integer getBookingId() { return BookingId; }
@@ -80,10 +77,6 @@ public class Ordering extends BaseDbo{
     public Boolean getIsDisabled() { return isDisabled; }
 
     public void setIsDisabled(Boolean isDisabled) { this.isDisabled = isDisabled; }
-
-
-    public Boolean getIsPaid() { return  isPaid; }
-
 
     public Boolean getIsPaid() { return isPaid; }
 
