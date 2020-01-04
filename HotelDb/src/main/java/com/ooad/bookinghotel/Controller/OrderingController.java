0215ca035b82cdc2850e2f15f1bb77c6a5d9242a
Mikie0520 <55621978+Mikie0520@users.noreply.github.com>
2020-01-04 21:27:45 +0800

    Merge branch 'master' of https://github.com/EvaWang/OOAD_Final
    # Conflicts:
    #	HotelDb/src/main/java/com/ooad/bookinghotel/HotelDb/Ordering.java

--- HotelDb/src/main/java/com/ooad/bookinghotel/Controller/OrderingController.java
+++ HotelDb/src/main/java/com/ooad/bookinghotel/Controller/OrderingController.java
@@ -29,9 +29,12 @@ public class OrderingController {
     @Autowired
     private OrderViewRepository orderViewRepository;
 
+    @Autowired
+    private HotelViewRepository hotelViewRepository;
+
     public Boolean CheckLegalDateRegion (Date StartDate,Date EndDate) {
         Date now = new Date();
-        if (StartDate.compareTo(EndDate) > 0) {
+        if (StartDate.compareTo(EndDate) >= 0) {
             System.out.println("Illegal time region");
             return false;
         } else if (StartDate.compareTo(now) < 0) {
@@ -77,6 +80,8 @@ public class OrderingController {
         return orderViewRepository.searchOrder(userId, orderId, pageable);
     }
 
+
+
     @GetMapping("/findMyOrderDetails/{userId}")
     Page<OrderView> findMyOrderDetails (@PathVariable int userId,
                                   @RequestParam(value="orderId", required = false) Integer orderId,
@@ -113,14 +118,9 @@ public class OrderingController {
         Date endDate = formatter1.parse(orderingObj.get("EndDate"));
 
         Ordering newOrdering = new Ordering();
-        newOrdering.setUserId(Integer.parseInt(orderingObj.get("UserId")));
-//        newOrdering.setDiscount(Double.parseDouble(orderingObj.get("Discount")));
-        newOrdering.setDiscount(1.0);
-        newOrdering.setStartDate(startDate);
-        newOrdering.setEndDate(endDate);
-        newOrdering.setMemo(orderingObj.get("Memo"));
-        newOrdering.setIsDisabled(false);
-        newOrdering.setIsPaid(false);
+        if (CheckLegalDateRegion(startDate,endDate) == false) {
+            return newOrdering;
+        }
         String HotelRoomTypes = orderingObj.get("HotelRoomTypes");
         String[] roomTypeList = HotelRoomTypes.split(",");
 
@@ -130,10 +130,49 @@ public class OrderingController {
         List<String> roomIdList_toList = Arrays.asList(roomTypeList);
 
         List<Integer> int_roomIdList_toList = new ArrayList<>();
-        for(String s : roomIdList_toList) int_roomIdList_toList.add(Integer.valueOf(s));
+
+        //Count the rooms needed for this order
+        Integer RoomsNeeded_Single = 0;
+        Integer RoomsNeeded_Double = 0;
+        Integer RoomsNeeded_Quad = 0;
+        for(String s : roomIdList_toList) {
+            int_roomIdList_toList.add(Integer.valueOf(s));
+            Integer Value = Integer.valueOf(s);
+            if (Value == 1) RoomsNeeded_Single = RoomsNeeded_Single + 1;
+            else if (Value == 2) RoomsNeeded_Double = RoomsNeeded_Double + 1;
+            else if (Value == 4) RoomsNeeded_Quad = RoomsNeeded_Quad + 1;
+        }
+
 
         List<HotelRoom> roomIdList_checked = new ArrayList<>();
+        List<Integer> HotelIds = new ArrayList<>();
+        HotelIds.add(hotelId);
         roomIdList_checked = hotelRoomRepository.findByRoomTypes(int_roomIdList_toList, hotelId);
+        List<HotelView> CheckIfExist = hotelViewRepository.findOne(HotelIds,orderingObj.get("StartDate"),orderingObj.get("EndDate"));
+
+        //Check if this hotel has enough rooms for this order
+        for (HotelView RoomView:CheckIfExist) {
+            if ((RoomView.getSingleRoom() - RoomView.getBookedSingleRoom()) < RoomsNeeded_Single) {
+                System.out.println("Not enough single rooms in this hotel between " + startDate + " ~ " + endDate);
+                return newOrdering;
+            } else if ((RoomView.getDoubleRoom() - RoomView.getBookedDoubleRoom()) < RoomsNeeded_Double) {
+                System.out.println("Not enough double rooms in this hotel between " + startDate + " ~ " + endDate);
+                return newOrdering;
+            } else if ( (RoomView.getQuadRoom() - RoomView.getBookedQuadRoom()) < RoomsNeeded_Quad) {
+                System.out.println("Not enough quad rooms in this hotel between " + startDate + " ~ " + endDate);
+                return newOrdering;
+            }
+        }
+
+        newOrdering.setUserId(Integer.parseInt(orderingObj.get("UserId")));
+//        newOrdering.setDiscount(Double.parseDouble(orderingObj.get("Discount")));
+        newOrdering.setDiscount(1.0);
+        newOrdering.setStartDate(startDate);
+        newOrdering.setEndDate(endDate);
+        newOrdering.setMemo(orderingObj.get("Memo"));
+        newOrdering.setIsDisabled(false);
+        newOrdering.setIsPaid(false);
+
         Integer Total = 0;
 
         Dictionary<Integer, Integer> roomDict = new Hashtable();
@@ -172,14 +211,16 @@ public class OrderingController {
     @PostMapping("/updateByDate/{id}")
     Ordering updateOrderingByDate(@RequestBody Map<String, String> orderingObj,@PathVariable int id) throws ParseException {
         SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy-MM-dd");
-
-        //Get information "startDate"
         Date startDate = formatter1.parse(orderingObj.get("StartDate"));
-
-        //Get information "endDate"
         Date endDate = formatter1.parse(orderingObj.get("EndDate"));
+        Date now = new Date();
 
-        Ordering originalOrdering = orderingRepository.findById(id).get();
+        Optional<Ordering> findOrder = orderingRepository.findById(id);
+        if(findOrder.isPresent() == false){
+            return findOrder.orElseThrow(() -> new NotFoundException(id));
+        }
+
+        Ordering originalOrdering = findOrder.get();
 
         //The order has been disabled
         if (originalOrdering.getIsDisabled() == true) {
@@ -196,6 +237,42 @@ public class OrderingController {
             return originalOrdering;
         }
 
+
+
+        List<Booking> BookingList = bookingRepository.findByOrderId(id);
+
+        Integer RoomsNeeded_Single = 0;
+        Integer RoomsNeeded_Double = 0;
+        Integer RoomsNeeded_Quad = 0;
+        Integer HotelId = 0;
+
+        for (Booking Book:BookingList) {
+            HotelId = Book.getHotelId();
+            HotelRoom hotelRoom = hotelRoomRepository.findById(Book.getHotelRoomId()).get();
+            Integer RoomType = hotelRoom.getRoomType();
+            if (RoomType == 1) {
+                RoomsNeeded_Single = RoomsNeeded_Single + 1;
+            } else if (RoomType == 2) {
+                RoomsNeeded_Double = RoomsNeeded_Double + 1;
+            } else if (RoomType == 4) {
+                RoomsNeeded_Quad = RoomsNeeded_Quad + 1;
+            }
+        }
+        List<Integer> HotelIds = new ArrayList<>();
+        HotelIds.add(HotelId);
+        List<HotelView> hotelViews = hotelViewRepository.findOne(HotelIds,orderingObj.get("StartDate"),orderingObj.get("EndDate"));
+        for (HotelView RoomView:hotelViews) {
+            if ((RoomView.getSingleRoom() - RoomView.getBookedSingleRoom()) < 0) {
+                System.out.println("Not enough single rooms in this hotel between " + startDate + " ~ " + endDate);
+                return originalOrdering;
+            } else if ((RoomView.getDoubleRoom() - RoomView.getBookedDoubleRoom()) < RoomsNeeded_Double) {
+                System.out.println("Not enough double rooms in this hotel between " + startDate + " ~ " + endDate);
+                return originalOrdering;
+            } else if ( (RoomView.getQuadRoom() - RoomView.getBookedQuadRoom()) < RoomsNeeded_Quad) {
+                System.out.println("Not enough quad rooms in this hotel between " + startDate + " ~ " + endDate);
+                return originalOrdering;
+            }
+        }
         Date originalStartDate = originalOrdering.getStartDate();
         Date originalEndDate = originalOrdering.getEndDate();
 
@@ -206,23 +283,44 @@ public class OrderingController {
         originalOrdering.setStartDate(startDate);
         originalOrdering.setEndDate(endDate);
 
+        System.out.println(startDate);
+
         Long days = (endDate.getTime() - startDate.getTime())/(24*60*60*1000);
+
+        System.out.println(startDate);
 
         int newDays = Math.toIntExact(days);
 
+        System.out.println(startDate);
+
         Integer originalTotal = originalOrdering.getTotal();
 
-        Integer newTotal = originalTotal*newDays/originalDays;
+        System.out.println(startDate);
 
+        Integer newTotal = 0;
+
+        if (originalDays == 0) {
+            newTotal = originalTotal*newDays;
+        } else {
+            newTotal = originalTotal*newDays/originalDays;
+        }
+
+        System.out.println(startDate);
         originalOrdering.setTotal(newTotal);
-
-        orderingRepository.save(originalOrdering);
-        return originalOrdering;
+        System.out.println(startDate);
+        return orderingRepository.save(originalOrdering);
     }
 
     @PostMapping("/updateByBooking/{id}")
-    Ordering updateOrderByDecreaseBooking(@RequestBody Map<String, String> orderingObj,@PathVariable int id) {
-        Ordering originalOrdering = orderingRepository.findById(id).get();
+    Ordering updateOrderByBooking(@RequestBody Map<String, String> orderingObj,@PathVariable int id) {
+        Date now = new Date();
+
+        Optional<Ordering> findOrder = orderingRepository.findById(id);
+        if(findOrder.isPresent() == false){
+            return findOrder.orElseThrow(() -> new NotFoundException(id));
+        }
+
+        Ordering originalOrdering = findOrder.get();
 
         if (originalOrdering.getIsPaid() == true) {
             System.out.println("This order has been Paid");
@@ -235,20 +333,34 @@ public class OrderingController {
         }
 
         Date startDate = originalOrdering.getStartDate();
+        if (CheckLegalDateRegion(now,startDate) == false) {
+            System.out.println("Can't modify this Order anymore");
+            return originalOrdering;
+        }
+
         Date endDate = originalOrdering.getEndDate();
         Long days = (endDate.getTime() - startDate.getTime())/(24*60*60*1000);
         int Days = Math.toIntExact(days);
 
         List<Booking> BookingList = bookingRepository.findByOrderId(id);
 
+        Integer Total = 0;
         for (Booking Book: BookingList) {
             if (Book.getIsDisabled() == true) {
                 BookingList.remove(Book);
+            }  else {
+                HotelRoom newHotelRoom = hotelRoomRepository.findById(Book.getHotelRoomId()).get();
+                Total = Total + newHotelRoom.getPrice();
             }
         }
+        if (Total == 0) {
+            originalOrdering.setIsDisabled(true);
+            return originalOrdering;
+        }
 
-
-
+        Total = Total * Days;
+        originalOrdering.setTotal(Total);
+        orderingRepository.save(originalOrdering);
         return originalOrdering;
     }
 
