7635b8a80f90ebedf04db023ec4ba6de7ca3222b
eva.wang <eva.wang@lileesystems.com>
2020-01-05 17:50:17 +0800

    init web server

---
+++ web/src/test/java/ooad/bookinghotel/web/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package ooad.bookinghotel.web.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
